d793efb35f000c24e7ba333aaab6384bfb1cf937
Unknown <jakash1997@gmail.com>
2019-06-05 10:36:39 +0530

    Update for gh-pages

---
+++ docs/precache-manifest.e07ce08ba351e86fa9d9f74cc89443e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eae8adb911862e9e3bfbab44507b281",
+    "url": "/superhero/index.html"
+  },
+  {
+    "revision": "0124b91ea614ca32842f",
+    "url": "/superhero/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5147461ae2b67ce2ddb7",
+    "url": "/superhero/static/js/2.e6c0efca.chunk.js"
+  },
+  {
+    "revision": "0124b91ea614ca32842f",
+    "url": "/superhero/static/js/main.8ce6a9eb.chunk.js"
+  },
+  {
+    "revision": "2da89f76f3fa528a6163",
+    "url": "/superhero/static/js/runtime~main.79f59e53.js"
+  }
+]);
